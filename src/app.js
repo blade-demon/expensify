b84c264e5608711920b7e83648e70d2c671ed2ba
blade-demon <xuziwei89@gmail.com>
2018-11-26 15:23:35 +0800

    add express server

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import AppRouter from './routers/AppRouter';
-import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
-import getVisibleExpenses from './selectors/expenses';
-import 'normalize.css/normalize.css';
-import './styles/styles.scss';
-import 'react-dates/lib/css/_datepicker.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import AppRouter from "./routers/AppRouter";
+import configureStore from "./store/configureStore";
+// import { addExpense } from "./actions/expenses";
+// import { setTextFilter } from "./actions/filters";
+// import getVisibleExpenses from "./selectors/expenses";
+import "normalize.css/normalize.css";
+import "./styles/styles.scss";
+import "react-dates/lib/css/_datepicker.css";
 
 const store = configureStore();
 
@@ -18,4 +18,4 @@ const jsx = (
   </Provider>
 );
 
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(jsx, document.getElementById("app"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,13 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = env => {
   const isProduction = env === "production";
+  const CSSExtract = new ExtractTextPlugin("styles.css");
+  const HtmlWebpack = new HtmlWebpackPlugin({
+    template: "src/index.html"
+  });
 
   return {
     entry: "./src/app.js",
@@ -13,18 +18,33 @@ module.exports = env => {
     module: {
       rules: [
         { loader: "babel-loader", test: /\.js$/, exclude: /node_modules/ },
-        { test: /\.s?css$/, use: ["style-loader", "css-loader", "sass-loader"] }
+        {
+          test: /\.s?css$/,
+          use: CSSExtract.extract({
+            use: [
+              {
+                loader: "css-loader",
+                options: {
+                  sourceMap: true
+                }
+              },
+              {
+                loader: "sass-loader",
+                options: {
+                  sourceMap: true
+                }
+              }
+            ]
+          })
+        }
       ]
     },
-    devtool: isProduction ? "source-map" : "cheap-module-eval-source-map",
+    devtool: isProduction ? "source-map" : "inline-source-map",
     devServer: {
       contentBase: path.join(__dirname, "public"),
+      // proxy requests  back to index.html
       historyApiFallback: true
     },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: "src/index.html"
-      })
-    ]
+    plugins: [HtmlWebpack, CSSExtract]
   };
 };
